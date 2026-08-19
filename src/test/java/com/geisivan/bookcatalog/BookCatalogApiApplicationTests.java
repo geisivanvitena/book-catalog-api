2f78ed543fd89d0f32ab58a5b2a5f458910fda95
Geisivan <gsv1205@yahoo.com>
2026-08-19 19:43:08 -0300

    chore: inicializa projeto

---
+++ src/test/java/com/geisivan/bookcatalog/BookCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geisivan.bookcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
